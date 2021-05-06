2c870cec4aa4b0ed2d97b02bc2a430a65d74cdc3
Braden <bcole0211@gmail.com>
2021-05-06 18:11:04 +1000

    updated heroService to get data from express server

--- teamselector/src/services/heroes.js
+++ teamselector/src/services/heroes.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3001/heroes';
-const teamURL = 'http://localhost:3001/team'
+const baseURL = 'http://localhost:3001/api/heroes';
+const teamURL = 'http://localhost:3001/api/team'
 
 const getAll = () => {
     const request = axios.get(baseURL);
